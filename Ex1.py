030acee9e430c4fe1ea71ca5d7cdb592e15db81a
Jorge <jorge.david.zepmed@gmail.com>
2017-07-18 16:58:53 -0500

    Empezamos Ex1

---
+++ Ex1.py
@@ -0,0 +1,14 @@
+#Ex1
+
+
+print "Hello word"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd uch rahter you 'not'."
+print 'I "said" do not touch this.'
+print "Prueba en Github."
+print "Segunda prueba en GitHub esta"
+#pruebas
+print "Ver si se nececita guardar en la pc"
---
+++ Ex3.py
@@ -0,0 +1,26 @@
+#Ex3
+
+print "I will now conut my chickens:"
+print "Hens", 25 + 30 / 6
+print  "Rossters", 100 - 25 * 3 % 4
+
+
+print "Now i will count the eggs"
+print "test ultimatum"
+
+print 3 + 2 + 1 -5 +4 % 2- 1 / 4 + 6
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+print 3 + 2 < 5 - 7
+print "What is 3 +2?", 3+2
+print "Whats is 5 - 7?", 5-7  #Nececita tener la coma para que pueda hacer la operacion  e imprimirlo alado
+
+
+print "Oh, that's why it's False."
+print "How about some more."
+print "Is it greater?", 5 > -2
+print "Is it grater or equial?", 5 > -2
+print "Is it less or equal??", 5 < -2
+print "Prueba de guardar git Plus"
+print "prueba de GitHub"

f1380cdfd304f7d85e08f278ddb9b060e05cf5b4
Jorge <jorge.david.zepmed@gmail.com>
2017-07-19 03:29:57 -0500

    Se agrego las especifiaciones del % con sus diversos usos.

--- Ex5.py
+++ Ex5.py
@@ -10,7 +10,7 @@ my_haire = "Brown"
 print "Let's talk about %s." % my_name            #ES LO MISMO SI EL "%" ESTA PEGADO O NO A LA VARIABLE
 print "He is %d metros tall." %my_height
 print "He's %d kilogram heavy " % my_weighy
-print "He's got %s eyes and %s hair" % (my_eyes, my_haire)
+print "He's got %s eyes and %s hair" % (my_eyes, my_haire)   #DEBE DE TENER LOS ( ) PARA QUE PUEDA CORRER
 
 
-print "If i add %d, %d, and %d I get %d." % (my_adge, my_height, my_weighy, my_adge + my_height + my_weighy)
+print "If i add %d, %r, and %r I get %r." % (my_adge, my_height, my_weighy, my_adge + my_height + my_weighy)

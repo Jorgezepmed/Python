ffd4c39e66618b6c63beaf3bad1f52545888b7c9
Jorge <jorge.david.zepmed@gmail.com>
2017-07-19 13:17:02 -0500

    Se modifico el Exy y se agregó el Ex7 sobre mas imprimrir

---
+++ Ex7.py
@@ -0,0 +1,22 @@
+#Ex7 MORE PRINTING
+
+print "Mary had a little lamb."
+print "Its fleece was while as %s." % 'snow'                #NO PRECICAMENTE DEBE DE SER VARIABLES DECLARADAS AL PONER % SE PUEDE PONERLA COM OSTRING ESPECIAFIACA AL LADO
+print "And everywhere thet Mary went."
+print "." * 10                                              # SE MULTIMPLICA POR 10 LOS PUNTOS EN EL MISMO RENGLON.
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+print end1 + end2 + end3 + end4 + end5 + end6,             #lA COMA HACE QUE EL SIGUIENTE PRINT NO HAGA UN SALTO DE RENGLON
+print end7 + end8 + end9 + end10 + end11 + end12

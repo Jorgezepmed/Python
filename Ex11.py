644fef4e9e80a5a7e42863d7900915d54845727e
Jorge <jorge.david.zepmed@gmail.com>
2017-07-20 01:26:42 -0500

    Se termino el Ex12, manera de hacer mas facil los inputs

--- Ex11.py
+++ Ex11.py
@@ -9,4 +9,4 @@ weight = raw_input()
 
 print "So, you're %s old, %r tall and %r heavy." % (age, height, weight)   #POR SER [%r] NOS LO VA A IMPRIMIR CON '' EL RESULTADO ENTRE EL TEXTO
 
-#SI FUERA UN [%s] DE STRING SI LO IMPRIMIRIA SIN LOS '' 
+#SI FUERA UN [%s] DE STRING SI LO IMPRIMIRIA SIN LOS ''
---
+++ Ex12.py
@@ -0,0 +1,7 @@
+#PROMPTING PEOPLE
+
+age = raw_input ("How old are you?")            #AL DECLARAR AL MISMO TIMPO LA VARIABLE CON EL INPUT SE HACE MAS CORTO
+height = raw_input ("How tall are you?")
+weight = raw_input ("How much do you weight?")
+
+print "So, you're %s old, %r tall and %r heavy." % (age, height, weight)
